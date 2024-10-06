0428197c689622f1c1b45e868b424d9b4b2f31a8
Max182xx <m.garciapro34@gmail.com>
2024-10-06 15:36:42 +0200

    TP04 UTILISATION DE METHODES STATIC EXISTANTES

---
+++ src/main/java/entites/TestMethodeStatic.java
@@ -0,0 +1,16 @@
+package entites;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+
+        String chaine = "12";
+        int nombre = Integer.parseInt(chaine);
+        System.out.println(chaine);
+
+        int a =34;
+        int b = 24;
+
+        int trouveMax = Integer.max( a, b);
+        System.out.println(trouveMax);
+    }
+}
