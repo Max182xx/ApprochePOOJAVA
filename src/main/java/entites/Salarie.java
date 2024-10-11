11f5e332f14467bb52a492805b9de4d3dcc8a0f5
Max182xx <m.garciapro34@gmail.com>
2024-10-11 11:17:01 +0200

    TP J1 Exercice ManipulationChaine termnié

--- src/main/java/entites/Salarie.java
+++ src/main/java/entites/Salarie.java
@@ -1,10 +1,19 @@
 package entites;
 
+// TP J1 Exercice ManipulationChaine
+
 public class Salarie {
 
     String nom;
     String prenom;
     double salaire;
 
+
+    // Constructeur de ma class
+    public Salarie(String nom, String prenom, double salaire){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.salaire = salaire;
+    }
 
 }
--- src/main/java/fr/diginamic/chaines/ManipulationChaine.java
+++ src/main/java/fr/diginamic/chaines/ManipulationChaine.java
@@ -1,19 +1,14 @@
 package fr.diginamic.chaines;
 
 
+import entites.Salarie;
 
-//8) Créez une classe Salarie dans le package entites avec 3 attributs
-//a. nom : String
-//b. prenom : String
-//c. salaire : double
-//9) A partir des 3 morceaux de chaine de caractères précédents, créez une instance de
-//Salarie :
-//a. Consigne : le nombre « 2 523.5» contient un espace qu’il faut supprimer en
-//utilisant la méthode replace(String, String) de la classe String
-//b. Astuce : pour transformer une chaine de caractères en double, utilisez la
-//méthode static de la classe Double : Double.parseDouble(String)
+// TP J1 Exercice ManipulationChaine
+
 public class ManipulationChaine {
     public static void main(String[] args) {
+
+        Salarie salaire = new Salarie("Durand", "Marcel", 2523.5);
 
         String chaine = "Durand;Marcel;2 523.5";
         char test = chaine.charAt(0);
@@ -22,12 +17,19 @@ public class ManipulationChaine {
         String majuscules = extraire.toUpperCase();
         String minuscules = extraire.toLowerCase();
         String[] morceaux = chaine.split(";");
+
+        String replace = chaine.replace(" ", "");
+        String salaireStr = morceaux[2].replace(" ", "");
+        Double enDouble = Double.parseDouble(salaireStr);
+
+        System.out.println("Extraire un chiffre d'une chaine de caractères :" + enDouble);
         System.out.println("Le prermier caractère est : " + test);
         System.out.println("La longueur de la chaine est : " + chaine.length());
         System.out.println("Afficher l’index du ; et le nom : " + indexChar + " " + extraire);
         System.out.println("Nom de famille ne majuscules : " + majuscules);
         System.out.println("Nom de famille ne minuscules : " + minuscules);
         System.out.println("Chaine en morceau : ");
+        System.out.println("Espace supprimé dans le chiffre 2 523.5 : " + replace);
         for (String partie : morceaux) {
             System.out.println(partie);
         }
