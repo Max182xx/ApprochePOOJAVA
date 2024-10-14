2d6ff8008ec6b93f7b29a835544899c2a5d4d93f
Max182xx <m.garciapro34@gmail.com>
2024-10-14 14:06:25 +0200

    Modif TP17  DATES ET CALENDAR

--- src/main/java/fr/diginamic/dates/TestCalendar.java
+++ src/main/java/fr/diginamic/dates/TestCalendar.java
@@ -12,25 +12,26 @@ public class TestCalendar  {
     public static void main(String[] args) throws Exception {
 
         Calendar cal = Calendar.getInstance();
-        cal.set(116,4,19,23,59,30);
+        cal.set(2016,4,19,23,59,30);
 
         Date date = cal.getTime();
-        System.out.println(date);
+        SimpleDateFormat timeFormat = new SimpleDateFormat("dd MMMM yyyy");
+        System.out.println(timeFormat.format(date));
 
-        SimpleDateFormat formateur = new SimpleDateFormat("EEEE dd MMMM yyyy HH:mm:ss", Locale.US);
-        System.out.println(formateur.format(new Date(2016, 4,19,23,59,30)));
+        SimpleDateFormat formateur = new SimpleDateFormat("yyyy MMMM dd HH:mm:ss", Locale.US);
+        System.out.println(formateur.format(new Date( 116,4,19,23,59, 30)));
 
-        SimpleDateFormat formateur2 = new SimpleDateFormat("EEEE dd MMMM yyyy HH:mm:ss", Locale.FRANCE);
-        System.out.println(formateur2.format(new Date(2016, 4,19,23,59,30)));
+        SimpleDateFormat formateur2 = new SimpleDateFormat("yyyy MMMM dd HH:mm:ss", Locale.FRANCE);
+        System.out.println(formateur2.format(new Date(116, 4,19,23,59,30)));
 
-        SimpleDateFormat formateur3 = new SimpleDateFormat("EEEE dd MMMM yyyy HH:mm:ss", Locale.CHINA);
-        System.out.println(formateur3.format(new Date(2016, 4,19,23,59,30)));
+        SimpleDateFormat formateur3 = new SimpleDateFormat("yyyy MMMM dd HH:mm:ss", Locale.CHINA);
+        System.out.println(formateur3.format(new Date(116, 4,19,23,59,30)));
 
-        SimpleDateFormat formateur4 = new SimpleDateFormat("EEEE dd MMMM yyyy HH:mm:ss", Locale.GERMAN);
-        System.out.println(formateur4.format(new Date(2016, 4,19,23,59,30)));
+        SimpleDateFormat formateur4 = new SimpleDateFormat("yyyy MMMM dd HH:mm:ss", Locale.GERMAN);
+        System.out.println(formateur4.format(new Date(116, 4,19,23,59,30)));
 
-        SimpleDateFormat formateur5 = new SimpleDateFormat("EEEE dd MMMM yyyy HH:mm:ss", Locale.forLanguageTag("ru"));
-        System.out.println(formateur5.format(new Date(2016, 4,19,23,59,30)));
+        SimpleDateFormat formateur5 = new SimpleDateFormat("yyyy MMMM dd HH:mm:ss", Locale.forLanguageTag("ru"));
+        System.out.println(formateur5.format(new Date(116, 4,19,23,59,30)));
 
     }
 }
